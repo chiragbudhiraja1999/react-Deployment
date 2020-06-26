131626597ebe578d68e44b9332a263614dc30e05
chirag Budhiraja <chiragbudhiraja005@gmail.com>
2020-06-26 12:26:55 +0530

    Updates

---
+++ precache-manifest.3f87d8f18caa2f927584f676fd52e02f.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ba8a3d264d47e8df1a0281362df93c",
+    "url": "/react-Deployment/index.html"
+  },
+  {
+    "revision": "060654a21b3007db5402",
+    "url": "/react-Deployment/static/css/2.f5852ac9.chunk.css"
+  },
+  {
+    "revision": "3f07f574d708988b36d7",
+    "url": "/react-Deployment/static/css/main.1764e982.chunk.css"
+  },
+  {
+    "revision": "060654a21b3007db5402",
+    "url": "/react-Deployment/static/js/2.42b9dedc.chunk.js"
+  },
+  {
+    "revision": "3f07f574d708988b36d7",
+    "url": "/react-Deployment/static/js/main.0514bdbb.chunk.js"
+  },
+  {
+    "revision": "88adae6fcb306f09fc89",
+    "url": "/react-Deployment/static/js/runtime~main.ba4c2396.js"
+  },
+  {
+    "revision": "0767d6d71dece2b7f6ff5606b666a1d3",
+    "url": "/react-Deployment/static/media/Capture.0767d6d7.png"
+  },
+  {
+    "revision": "bfb0988f8aa42ad2d99f5d93ad019afa",
+    "url": "/react-Deployment/static/media/Capture1.bfb0988f.png"
+  },
+  {
+    "revision": "482cf0d935393d594393bcfe12fabe45",
+    "url": "/react-Deployment/static/media/Cloud.482cf0d9.svg"
+  },
+  {
+    "revision": "a857ad855743c736dc0ecf7ea77f2262",
+    "url": "/react-Deployment/static/media/DJ.a857ad85.png"
+  },
+  {
+    "revision": "9b39e3487a1182e6a4eadd5ccbb2fc88",
+    "url": "/react-Deployment/static/media/Event.9b39e348.png"
+  },
+  {
+    "revision": "5642c97d5c5a89a5a71816566290fbc3",
+    "url": "/react-Deployment/static/media/Exlcusive1.5642c97d.png"
+  },
+  {
+    "revision": "19e70d0f40d7097aaf5eeef2f2d44b03",
+    "url": "/react-Deployment/static/media/Gobold-Extra2-Italic.ttf.19e70d0f.svg"
+  },
+  {
+    "revision": "952081a66c3c9d5b6ae249131686f1eb",
+    "url": "/react-Deployment/static/media/Gobold-Extra2-Italic.ttf.952081a6.eot"
+  },
+  {
+    "revision": "b950b13d135bd5c15fa5746d4548c71d",
+    "url": "/react-Deployment/static/media/Gobold-Extra2-Italic.ttf.b950b13d.woff"
+  },
+  {
+    "revision": "e66955486a60d1e8cb67f29993904290",
+    "url": "/react-Deployment/static/media/Merchandise1.e6695548.png"
+  },
+  {
+    "revision": "e76068206850b9513ddb869d1eb23470",
+    "url": "/react-Deployment/static/media/Poster1.e7606820.png"
+  },
+  {
+    "revision": "16cd9f451d4db6f7e8eecd4630429c7e",
+    "url": "/react-Deployment/static/media/Poster2.16cd9f45.png"
+  },
+  {
+    "revision": "1a244899afa72ea7c5449238c2ab5cde",
+    "url": "/react-Deployment/static/media/Product1.1a244899.png"
+  },
+  {
+    "revision": "3bf299c539a2422623bb245b31310a80",
+    "url": "/react-Deployment/static/media/Product2.3bf299c5.png"
+  },
+  {
+    "revision": "a6166c73296adf93f487f0cc1386902d",
+    "url": "/react-Deployment/static/media/Shazam.a6166c73.svg"
+  },
+  {
+    "revision": "5da3012a29e7aeedd1402865ab0f3908",
+    "url": "/react-Deployment/static/media/Shoes.5da3012a.png"
+  },
+  {
+    "revision": "33b0b48efe6f68b37abc57583bc5b518",
+    "url": "/react-Deployment/static/media/Spotify.33b0b48e.svg"
+  },
+  {
+    "revision": "04a29d32b15abadb04030e124799661f",
+    "url": "/react-Deployment/static/media/Sukhbir.04a29d32.png"
+  },
+  {
+    "revision": "a13f0ddf4151eb2d521ae6a457cb0d63",
+    "url": "/react-Deployment/static/media/Work2.a13f0ddf.png"
+  },
+  {
+    "revision": "b6e8ed03964ab19acc9720e251088eb2",
+    "url": "/react-Deployment/static/media/Youtube.b6e8ed03.svg"
+  },
+  {
+    "revision": "1dfc66c75d0bc90cfbffb5076a7c6f90",
+    "url": "/react-Deployment/static/media/muli-latin-200.1dfc66c7.woff2"
+  },
+  {
+    "revision": "b79246dbe57f609f29b0d4dbb54d99d3",
+    "url": "/react-Deployment/static/media/muli-latin-200.b79246db.woff"
+  },
+  {
+    "revision": "787c91cfe2aa739cebd29833fe1dad24",
+    "url": "/react-Deployment/static/media/muli-latin-200italic.787c91cf.woff"
+  },
+  {
+    "revision": "ed5f4d703d12d159cda99d1cb125c217",
+    "url": "/react-Deployment/static/media/muli-latin-200italic.ed5f4d70.woff2"
+  },
+  {
+    "revision": "6e03d5eb9f5824d4d419f37d4b251346",
+    "url": "/react-Deployment/static/media/muli-latin-300.6e03d5eb.woff"
+  },
+  {
+    "revision": "73d23299b229dfb09a7ee6a0e17cbb0b",
+    "url": "/react-Deployment/static/media/muli-latin-300.73d23299.woff2"
+  },
+  {
+    "revision": "25532f4b3540b5532803e457d0a6a192",
+    "url": "/react-Deployment/static/media/muli-latin-300italic.25532f4b.woff"
+  },
+  {
+    "revision": "49ef162ab65bc9d5186133d5da5b1530",
+    "url": "/react-Deployment/static/media/muli-latin-300italic.49ef162a.woff2"
+  },
+  {
+    "revision": "8160dac0c3b1b1cedd4299fe5fff9b6b",
+    "url": "/react-Deployment/static/media/muli-latin-400.8160dac0.woff2"
+  },
+  {
+    "revision": "b0f17b38ff3123d07235537f07affb6b",
+    "url": "/react-Deployment/static/media/muli-latin-400.b0f17b38.woff"
+  },
+  {
+    "revision": "688fb50a683fa1eb3c305f13d60ad0b5",
+    "url": "/react-Deployment/static/media/muli-latin-400italic.688fb50a.woff"
+  },
+  {
+    "revision": "730c6bfaeefe6acc35962b09a3216780",
+    "url": "/react-Deployment/static/media/muli-latin-400italic.730c6bfa.woff2"
+  },
+  {
+    "revision": "0f2fbd56c70276163bf298818df7663c",
+    "url": "/react-Deployment/static/media/muli-latin-600.0f2fbd56.woff"
+  },
+  {
+    "revision": "4273185a48871f908fe3dcf3ae0b394e",
+    "url": "/react-Deployment/static/media/muli-latin-600.4273185a.woff2"
+  },
+  {
+    "revision": "7567e1367478a68772c8b79178e5fe40",
+    "url": "/react-Deployment/static/media/muli-latin-600italic.7567e136.woff2"
+  },
+  {
+    "revision": "9897cb2b2dd16cce16d858134ec4991a",
+    "url": "/react-Deployment/static/media/muli-latin-600italic.9897cb2b.woff"
+  },
+  {
+    "revision": "a6600f60df53aa945a5e7ddbfba1f813",
+    "url": "/react-Deployment/static/media/muli-latin-700.a6600f60.woff"
+  },
+  {
+    "revision": "d3703a0d8c46d70b7328668f451b72bd",
+    "url": "/react-Deployment/static/media/muli-latin-700.d3703a0d.woff2"
+  },
+  {
+    "revision": "00211c4acc40741e99c65632d29bcf1c",
+    "url": "/react-Deployment/static/media/muli-latin-700italic.00211c4a.woff"
+  },
+  {
+    "revision": "af763a17a259b819d020f2eff035d919",
+    "url": "/react-Deployment/static/media/muli-latin-700italic.af763a17.woff2"
+  },
+  {
+    "revision": "47fd62e6a0a09afa9e887c591575a9fa",
+    "url": "/react-Deployment/static/media/muli-latin-800.47fd62e6.woff"
+  },
+  {
+    "revision": "ab72c9c1fb45b2f7a9024ccdd16db9f7",
+    "url": "/react-Deployment/static/media/muli-latin-800.ab72c9c1.woff2"
+  },
+  {
+    "revision": "33637beba96ae8534f650cff84fd6d2d",
+    "url": "/react-Deployment/static/media/muli-latin-800italic.33637beb.woff2"
+  },
+  {
+    "revision": "a1e64515397a4c9b3d595405dbd42b47",
+    "url": "/react-Deployment/static/media/muli-latin-800italic.a1e64515.woff"
+  },
+  {
+    "revision": "37700966bbf1ee00e8117d24894a626e",
+    "url": "/react-Deployment/static/media/muli-latin-900.37700966.woff2"
+  },
+  {
+    "revision": "66c9d5ed7f4803424dda9b691851b222",
+    "url": "/react-Deployment/static/media/muli-latin-900.66c9d5ed.woff"
+  },
+  {
+    "revision": "3b4a9a0ecbd7b6d3e91054d1592f1036",
+    "url": "/react-Deployment/static/media/muli-latin-900italic.3b4a9a0e.woff"
+  },
+  {
+    "revision": "f99056b607ef79e25593f04506f5f744",
+    "url": "/react-Deployment/static/media/muli-latin-900italic.f99056b6.woff2"
+  },
+  {
+    "revision": "3d12f291a14aa335c78bf9a495914810",
+    "url": "/react-Deployment/static/media/sukhbir_profile.3d12f291.png"
+  }
+]);
